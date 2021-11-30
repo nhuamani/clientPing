b1c7196beca34601588b466d0dcaf7a0bfa98a8e
nhuamani <nhuamani.c7@gmail.com>
2021-11-30 09:02:31 -0500

    Implementación de service FILTER

--- src/app/service/server.service.ts
+++ src/app/service/server.service.ts
@@ -5,6 +5,7 @@ import { tap, catchError } from 'rxjs/operators';
 
 import { CustomResponse } from '../interface/custom-response';
 import { Server } from '../interface/server';
+import { Status } from '../enum/status.enum';
 
 
 @Injectable({
@@ -39,6 +40,27 @@ export class ServerService {
       .pipe(
         tap(console.log),
         catchError(this.handleError)
+      );
+
+
+  filter$ = (status: Status, response: CustomResponse) => <Observable<CustomResponse>>
+    new Observable<CustomResponse>(
+      suscriber => {
+        console.log(response);
+        suscriber.next(
+          status === Status.ALL ? { ...response, message: `Servers filtered by ${status} status`} :
+          {
+            ...response,
+            message: response.data.servers.filter(server => server.status === status).length > 0 ?
+            `Servers filtered by ${status === Status.SERVER_UP ? 'SERVER UP' : 'SERVER DOWN'} status` :
+            `No servers of ${status} found`,
+            data: {
+              servers: response.data.servers.filter(server => server.status === status)
+            }
+          }
+        )
+      }
+
       );
 
 
