0608494ad6b9745b41d4f224f14dbb1e14a709af
nhuamani <nhuamani.c7@gmail.com>
2021-12-08 08:38:47 -0500

    Injección de Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
+import { DataState } from './enum/data-state.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'clientPing';
+export class AppComponent implements OnInit {
+  appState$: Observable<AppState<CustomResponse>>;
+
+  constructor(private serverService: ServerService) {}
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$.pipe(
+      map((response) => {
+        return { dataState: DataState.LOADED_STATE, appData: response };
+      }),
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error: error });
+      })
+    );
+  }
 }
