32ccac2023a7a7aea3ed255866f0d4a9083e46c2
nhuamani <nhuamani.c7@gmail.com>
2022-01-16 16:10:41 -0500

    Cambio de estado LOADING, LOADED and ERROR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { ServerService } from './service/server.service';
 })
 export class AppComponent implements OnInit {
   appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
 
   constructor(private serverService: ServerService) {}
 
