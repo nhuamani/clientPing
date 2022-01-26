341b5879488c2d8c88b8df806629b3c6228746d7
nhuamani <nhuamani.c7@gmail.com>
2022-01-26 18:32:13 -0500

    Pingings Local Servers, ip usadas en la red local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,19 +18,41 @@ export class AppComponent implements OnInit {
   readonly DataState = DataState;
   readonly Status = Status;
   private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
   filterStatus$ = this.filterSubject.asObservable();
 
   constructor(private serverService: ServerService) {}
 
   ngOnInit(): void {
-    this.appState$ = this.serverService.servers$.pipe(
-      map((response) => {
-        return { dataState: DataState.LOADED_STATE, appData: response };
-      }),
-      startWith({ dataState: DataState.LOADING_STATE }),
-      catchError((error: string) => {
-        return of({ dataState: DataState.ERROR_STATE, error: error });
-      })
-    );
+    this.appState$ = this.serverService.servers$
+      .pipe(
+        map((response) => {
+          this.dataSubject.next(response);
+          return { dataState: DataState.LOADED_STATE, appData: response };
+        }),
+        startWith({ dataState: DataState.LOADING_STATE }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error: error });
+        })
+      );
+  }
+
+  
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress)
+      .pipe(
+        map((response) => {
+          const index = this.dataSubject.value.data.servers.findIndex(server => server.id === response.data.server.id)
+          this.dataSubject.value.data.servers[index] = response.data.server;
+          this.filterSubject.next('');
+          return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value };
+        }),
+        startWith({ dataState: DataState.LOADED_STATE, appData: this.dataSubject.value }),
+        catchError((error: string) => {
+          this.filterSubject.next('');
+          return of({ dataState: DataState.ERROR_STATE, error: error });
+        })
+      );
   }
 }
